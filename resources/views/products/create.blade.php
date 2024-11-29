c8b6992f768277d1725893ea5b3832759cc33dcc
Gediminas <gediss.lolas78@gmail.com>
2024-11-29 17:30:18 +0200

    changed form components

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,48 +1,32 @@
 <x-layout>
-    <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md mt-5">
+    <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md mt-20">
         <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">Add New Product</h1>
         <form class="space-y-4" method="POST" action="/products">
             @csrf
             <div>
-                <label for="product_name" class="block text-sm font-medium text-gray-700">Product Name</label>
-                <input type="text" id="product_name" name="product_name" required
-                    class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-xs focus:outline-hidden focus:ring-indigo-500 focus:border-indigo-500">
+                <x-label for="product_name">Product Name</x-label>
+                <x-input type="text" id="product_name" name="product_name" required/>
             </div>
-            @error('product_name')
-                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-            @enderror
+            <x-errors name="product_name"/>
             <div>
-                <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
-                <textarea id="description" name="description" rows="3" required
-                    class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-xs focus:outline-hidden focus:ring-indigo-500 focus:border-indigo-500"></textarea>
+                <x-label for="description">Description</x-label>
+                <x-textarea id="description" name="description" rows="3" required></x-textarea>
             </div>
-            @error('description')
-                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-            @enderror
+            <x-errors name="description"/>
             <div>
-                <label for="image" class="block text-sm font-medium text-gray-700">Product Image URL</label>
-                <input type="text" id="image" name="image" required
-                    class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-xs focus:outline-hidden focus:ring-indigo-500 focus:border-indigo-500">
+                <x-label for="image">Product Image URL</x-label>
+                <x-input type="text" id="image" name="image" required/>
             </div>
-            @error('image')
-                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-            @enderror
+           <x-errors name="image"/>
             <div>
-                <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
-                <div class="mt-1 relative rounded-md shadow-xs">
-
-                    <input type="number" id="price" name="price" min="0" step="0.01" required
-                        class="block w-full pl-4 pr-2 py-2 bg-white border border-gray-300 rounded-md shadow-xs focus:outline-hidden focus:ring-indigo-500 focus:border-indigo-500">
+                <x-label for="price">Price</x-label>
+                <div>
+                    <x-input type="number" id="price" name="price" required/>
                 </div>
             </div>
-            @error('price')
-                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-            @enderror
+            <x-errors name="price"/>
             <div>
-                <button type="submit"
-                    class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-xs text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Add Product
-                </button>
+                <x-button type="submit" class="px-4 py-2">Add Product </x-button>
             </div>
         </form>
     </div>
