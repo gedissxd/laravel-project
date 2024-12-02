f251b67772d074b51d54e8eebe6211a534d6c307
Gediminas <gediss.lolas78@gmail.com>
2024-12-02 03:24:01 +0200

    add auth on create button

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,8 @@
 <x-layout>
     <div class="mx-auto px-4 py-8 max-w-[1300px]">
+        @auth
         <x-button class="w-[200px] p-4 mb-5" href="/products/create">Create Product</x-button>
+        @endauth
         <div class="grid lg:grid-cols-4 gap-6 md:grid-cols-2 ">
             @foreach ($products as $product)
             <x-product-block :product="$product"/>
