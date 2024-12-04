25e1ddf64afd6cbaaf1650bc1d620732307597da
Gediminas <gediss.lolas78@gmail.com>
2024-12-04 02:41:53 +0200

    idk

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        
     ],
-    build: {
-        outDir: 'public/build',
-        assetsDir: '',
-        manifest: true,
-    },
 });
